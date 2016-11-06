ebfe3f75febc252e86e901d0cdda64ec04bcb556
CristianGarciaRamirez <a291505@uach.mx>
2016-11-05 18:35:13 -0700

    faltaron las carpetas :v

---
+++ js/ind.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(window).load(function() {
+  	$(".loader").fadeOut("slow");
+  });
+
+  $("#ir").click(function() {
+    var nombre = $("#name").val();
+    $("#distribuciones").html('<b>Bienvenido, '+nombre+'!</b></br><a href="DistribucionB.html"><button id="ventanaGenero">D.Binominal</button></a>'+
+    '   <a href="DistribucionM.html"><button id="ventanaConsoles">D.Multinomial</button></a></br></br>'+
+    '<a href="DistribucionBN.html"><button id="ventanaVideogames">D.Binomial Negativa</button></a>'+
+    '   <a href="DistribucionP.html"><button id="ventanaSales">D.Poisson</button></br></br></a>'+
+    '</br>');
+  });
+});
