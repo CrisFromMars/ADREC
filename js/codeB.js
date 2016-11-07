f7b1fb9e24f8438c5583fb0d75ccb820a0796733
CristianGarciaRamirez <a291505@uach.mx>
2016-11-06 18:21:59 -0700

    Cambios en B y BN
    Cambios en Binomial y Binomial Negativa:
    
    - Funcionando correctamente todas las operaciónes de B y BN.
    - Eliminado el boton de ayuda.
    - Eliminado el boton de mas información.
    - Agregado un boton de ayuda individual para cada campo de texto.
    - Agregado el boton de resultado.

--- js/codeB.js
+++ js/codeB.js
@@ -1,87 +1,41 @@
 $(document).ready(function() {
 
-    var help = 'AYUDA AL USUARIO <br> <br>'+
-              'n es el numero total de ensayos, es un numero entero positivo.'+
-              '</br></br></br>k es el numero de especifico de ensayos a realizar. De igual manera es numero entero y debe ser menor o igual a n.'+
-              '</br></br></br>p es la probabilidad de exito. Debe ser una cantidad entre 0 y 1.'+
-              '</br></br></br>q es la probabilidad de fracaso. Debe ser una cantidad entre 0 y 1.';
+    $('#calcularB').click(function(){
+                console.log(n);
+              	var n=parseInt(document.getElementById("n").value);
+              	var r=parseInt(document.getElementById("r").value);
+              	var p=document.getElementById("p").value;
+              	if(p<0||p>1){alert("'p' debe ser un número entre 0 y 1");return;}
+              	if(r>n){alert("'x' debe ser menor o igual a 'n' ");return;}
+              	var nf=1;
+              	var rf=1;
+              	var nrf=1;
+              	for(var k=r;k>=1;k--){
+              		rf=rf*k;
+              	}
+              	for(var i=n;i>=1;i--){
+              			nf=nf*i;
+              	}
 
-    function nPk(ene, kha)
-    {
-      var ret = 1;
-      if (kha == 0) return 1;
-      else
-      {
-        if (kha < ene)
-        {
-          console.error("k es menor a n");
-          return "Ingrese nuevamente los valores";
-        }
-        else
-        {
-          for(i=1; i<=kha; i++)
-          {
-            ret *= ene;
-            ene--;
-          }
-        }
-      }//fin else
-      return ret;
-    }
+              	for(var j=(n-r);j>=1;j--){
+              		nrf=nrf*j;
+              	}
+              	var npr=nf/nrf;
+              	var ncr=npr/rf;
+              	var des=ncr*Math.pow(p,r)*Math.pow((1-p),(n-r));
 
-  function nCk(n, k)
-  {
-    if(k > n/2) k = n-k;
-    var f = nPk(n, k);
-    var g = factorial(k);
-    var h = f/g;
+              	document.getElementById("result1").value=Math.round(des*10000)/10000;
+              });
 
-    return Math.floor(h);
-  }
+      $('#infoN').click(function(){
+              document.getElementById("textN").innerHTML = "n es el numero (entero y positivo) total de ensayos.";
+          });
 
-  function factorial(x)
-  {
-    x = Math.floor(x);
-    var ret = 1;
-    if (x<0) {
-     alert(x+" No es un numero factorial valido.")
-    }
-    //else if(x == 0){
-      //    }
-    else
-    {
-      while(x>0)
-      {
-        ret *= x;
-        x--;
-      }
-     }//end else
-    return ret;
- }
+      $('#infoX').click(function(){
+              document.getElementById("textX").innerHTML = "x es el numero (entero y positivo) de ensayos a realizar. Tiene que ser menor o igual a n.";
+          });
 
- function calcular(n1,n2,n3,n4)
- {
-   var resultado = 0;
-
-   resultado = nCk(n1,n2) * (n3^n2) * (n4^(n1-n2));
-
-   return resultado;
- }
-
-    // Boton ayuda ******************
-    $("#ayuda").click(function() {
-      $("#contenido").html(help);
-
-    });
-
-    $("#calculaB").click(function() {
-      var n = $("#n").val();
-      var k = $("#k").val();
-      var p = $("#p").val();
-      var q = $("#q").val();
-
-      alert(nPk(n,k))
-      //$('#resultados').text(calcular(n,k,p,q));
-    });
-
+      $('#infoP').click(function(){
+              document.getElementById("textP").innerHTML = "p es la probabilidad de exito. Debe ser una cantidad entre 0 y 1.";
+          });
 });
--- js/codeBN.js
+++ js/codeBN.js
@@ -1,12 +1,71 @@
-// Boton Lista/Mostrar *****************
 $(document).ready(function() {
-  var help = 'AYUDA AL USUARIO <br> <br>'+
-            'n es el numero total de ensayos, es un numero entero positivo.'+
-            '</br></br></br>k es el numero de especifico de ensayos a realizar. De igual manera es numero entero y debe ser menor o igual a n.'+
-            '</br></br></br>p es la probabilidad de exito. Debe ser una cantidad entre 0 y 1.'+
-            '</br></br></br>q es la probabilidad de fracaso. Debe ser una cantidad entre 0 y 1. ';
-  $("#ayuda").click(function() {
-    $("#contenido").html(help);
-  });
+
+    $('#calcularBN').click(function(){
+
+        console.log("Hola :v");
+        var n=parseInt($('#n').val());
+        var k=parseInt($('#k').val());
+        var p=parseFloat($('#p').val());
+
+        if(p>1)
+        {
+          alert("'p' debe ser un número entre 0 y 1");
+        }
+        else if(k>n)
+        {
+          alert("'k' debe ser menor que 'n'");
+        }
+        else
+        {
+          var a=n-1;
+          var b=k-1;
+
+        //(n-1)!
+          var z=1;
+          for(var i=a; i>=1; i--)
+          {
+            z=z*i;
+          }
+
+        //(k-1)!
+          var t=1;
+          for(var j=b; j>=1; j--)
+          {
+            t=t*j;
+          }
+
+        //((n-1)-(k-1))!
+          var x=a-b;
+          var s=1;
+          for(hj=x; hj>=1; hj--)
+          {
+            s=s*hj;
+          }
+
+          var c=z/s;
+
+        //((n-1)!/((n-1)-(k-1))!)/(k-1)! = d ; p^k = e ; 1-p=f  ; n-k=g
+          var d=c/t;
+          var e=Math.pow(p,k);
+          var f=1-p;
+          var g=n-k;
+          var h=Math.pow(f,g);
+          var res=d*e*h;
+          $('#result1').val(Math.round((res)*100000)/100000);
+        }
+
+    });
+
+    $('#infoN').click(function(){
+            document.getElementById("textN").innerHTML = "n es el numero (entero y positivo) total de ensayos.";
+        });
+
+    $('#infoK').click(function(){
+            document.getElementById("textK").innerHTML = "k es el numero (entero y positivo) de ensayos a realizar. Tiene que ser menor o igual a n.";
+        });
+
+    $('#infoP').click(function(){
+            document.getElementById("textP").innerHTML = "p es la probabilidad de exito. Debe ser una cantidad entre 0 y 1.";
+        });
 
 });
